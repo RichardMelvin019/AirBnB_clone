0a042c3d56eaaf8142a5977df24ff02ce06fc1bf
RichardMelvin019 <richardmelvin019@gmail.com>
2023-11-12 14:32:27 -0800

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """__init__ method. """
-        if len(kwargs) == 0:
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
