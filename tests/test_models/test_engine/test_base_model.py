c6238a64dd5291de9bdd381ebc641e1be1c383fd
vivianokose <vivianokose@gmail.com>
2023-11-12 10:10:19 -0800

    Air BnB Clone Tests

---
+++ tests/test_models/test_engine/test_base_model.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Unittest BaseModel. """
+
+import unittest
+import inspect
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """ Test for class BaseModel"""
+
+    def setup(self):
+        """setup. """
+        self.model = BaseModel()
+
+    def test_class_docstring(self):
+        """checks if class has docstring. """
+        msg = "BaseModel class does not have docstring"
+        self.assertIsNotNone(BaseModel.__doc__, msg)
+
+    def test_method_docstring(self):
+        """checks if class methods have docstring. """
+        methods = inspect.grtmembers(BasModel, predicate=inspect.ismethod)
+        msg = "method should have a dostring"
+        for name, method in methods:
+            with self.subTest(method=name):
+                self.assertIsNotNone(method.__doc__, f"{name} {msg}")
+
+    def test_module_docstring(self):
+        """check if modules have docstring. """
+        msg = "modules does not have docstring"
+        self.assertIsNotNone(models.base_model.__doc__, msg)
+
+    def test__init__(self):
+        """test if an obj is a type BaseModel. """
+        self.assertIsInstance(self.model, BaseModel)
+
+    def test_id(self):
+        """test that id is Unique. """
+        objectid = BaseModel()
+        objectid_2 = BaseModel()
+        self.assertNotEqual(objectid.id, objectid_2.id)
+
+    def test_str(self):
+        """check if the output of stris in
+        the specified format."""
+        object_str = "[BaseModel], ({0}), {1}".format(
+                self.model.id, self.model.__dict__
+                )
+        self.assertEqual(str(self.model), object_str)
+
+    def tets_save(self):
+        "check is date updated when save"
+        first_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(first_updated_at, self.model.updated_at)
+
+    def test_to_dict(self):
+        """check if to_dict returns a dictionary, if add a class
+        key with class name of the object and if updated_at and
+        created_at are converted to string object in ISO format."""
+        model_dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertIn('__class__', model_dict)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertIn('id', model_dict)
+        self.assertIn('created_at', model_dict)
+        self.assertIn('updated_at', model_dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""test for file storage"""
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """this will test the FileStorage"""
+
+    def setup(self):
+        """Sets up test methods."""
+        self.storage = FileStorage()
+
+    def test_all(self):
+        """tests if all works in File Storage"""
+        objects = self.storage.all()
+        self.assertIsInstance(objects, dict)
+        self.assertEqual(objects, {})
+
+    def test_new(self):
+        """test when new is created"""
+        model = BaseModel()
+        self.storage.new(model)
+        objects = self.storage.all()
+        self.assertIn(f"{model.__class__.__name__}.{model.id}", objects)
+
+    def test_save(self):
+        """test when file is saved"""
+        model = BaseModel()
+        self.storage.new(model)
+        self.storage.save()
+        with open("file.json", "r", encoding="utf-8") as f:
+            saved_data = json.load(f)
+        self.assertIn(f"{model.__class__.__name__}.{model.id}", saved_data
+
+    def test_reload(self):
+        """test reload"""
+        model = BaseModel()
+        self.storage.new(model)
+        self.storage.save()
+
+        # Clear existing objects and reload
+        self.storage.__objects = {}
+        self.storage.reload()
+        objects = self.storage.all()
+        self.assertIn(f"{model.__class__.__name__}.{model.id}", objects)
+
+
+if __name__ == "__main__":
+    unittest.main()
