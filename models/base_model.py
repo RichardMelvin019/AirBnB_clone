460b5462924bfed24be8ba815a3a34150519839e
RichardMelvin019 <richardmelvin019@gmail.com>
2023-11-14 03:26:06 -0800

    BaseModel updated

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,9 @@ class BaseModel():
 
     def __str__(self):
         """Prints print: [<class name>] (<self.id>) <self.__dict__> """
-        return f"[{self.__class__.__name__}], ({self.id}), {self.__dict__}"
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+                )
 
     def save(self):
         """Updates the attribute updated_at with the current datetime. """
